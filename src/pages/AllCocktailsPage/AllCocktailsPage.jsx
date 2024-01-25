87be653451ab41f5e6fd76bc8abe0b49456785ef
Maria Frih <maria.frih@gmail.com>
2024-01-25 16:36:59 +0100

    Revert "adds styling to homepage and changes the look of the navbar between the HP and other pages" Reverted commit This reverts commit 048edc0267a683da46cb27f496647bc0e9e13134.

--- src/pages/AllCocktailsPage/AllCocktailsPage.jsx
+++ src/pages/AllCocktailsPage/AllCocktailsPage.jsx
@@ -1,65 +1,48 @@
-import { useState } from "react";
+import React from "react";
 import "./AllCocktailsPage.css";
 
 function AllCocktailsPage() {
-  const [value, setValue] = useState(0);
   return (
     <div>
       <h2>
         Let our wonderful Mocktail Bartender make the perfect cocktail for you!
       </h2>
       <h3>
-        "Hi I'm Maria Garita. It's quite easy, just let me know what kind of the
-        tastes do you fancy and I'll try and imagine the tastiest alcohol free
+        "Hi I'm Mary Garita. It's quite easy, just let me know what kind the
+        tastes you fancy and I'll try and imagine the tastiest alcohol free
         cocktail for you. I promise, you'll find it mind blowing"
       </h3>
       <p>Use the sliders to finetune your cocktail:</p>
       <div className="all-sliders">
         <div className="one-slider">
           <div>
-            <label for="sour-sweet">Sourn vs. Sweet</label>
+            <label for="sourness">Sourness vs. Sweetness</label>
           </div>
           <div>
             <input
               className="slider-input"
               type="range"
-              id="sour-sweet"
-              name="sour-sweet"
+              id="sourness"
+              name="sourness"
               min="0"
               max="10"
-              value={value}
+              // value="5"
               step="1"
+              list="sour-markers"
             />
-          </div>
-          <div>
-            <label for="bitter">Bitter</label>
-          </div>
-          <div>
-            <input
-              className="slider-input"
-              type="range"
-              id="bitter"
-              name="bitter"
-              min="0"
-              max="10"
-              value={value}
-              step="1"
-            />
-          </div>
-          <div>
-            <label for="salty">Salty</label>
-          </div>
-          <div>
-            <input
-              className="slider-input"
-              type="range"
-              id="salty"
-              name="salty"
-              min="0"
-              max="10"
-              value={value}
-              step="1"
-            />
+            <datalist className="datalist" id="sour-markers">
+              <option value="0" label="very sour!"></option>
+              <option value="1"></option>
+              <option value="2"></option>
+              <option value="3"></option>
+              <option value="4"></option>
+              <option value="5"></option>
+              <option value="6"></option>
+              <option value="7"></option>
+              <option value="8"></option>
+              <option value="9"></option>
+              <option value="10" label="very sweet!"></option>
+            </datalist>
           </div>
         </div>
       </div>
